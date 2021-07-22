3f76353fdbb6291bf7bd561862f368dbb08fa91f
BillTran <trananh211@gmail.com>
2021-07-22 10:18:07 +0700

    test

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,6 +2,6 @@
     <strong>Copyright &copy; 2014-2020 <a href="#">AdminLTE</a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
-        <b>Version</b> 1.0.0-beta
+        <b>Version</b> 1.0.1-beta
     </div>
 </footer>
